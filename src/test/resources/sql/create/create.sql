1cc5d6315c44ad80881120629a4b935efb75475d
zhaiyz <hanxue_lang@foxmail.com>
2013-08-06 00:22:13 +0800

    初始导入

---
+++ src/test/resources/sql/create/create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE person (
+	id int not null auto_increment,
+	name varchar(32) not null,
+	password varchar(32) not null,
+	primary key (id)
+);
---
+++ src/test/resources/sql/init/init.sql
@@ -0,0 +1,1 @@
+INSERT INTO person (name, password) VALUES ('zhaiyz', "Hello, world!");
